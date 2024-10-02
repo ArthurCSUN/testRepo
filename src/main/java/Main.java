3031335efd1af2ea3caab4153825ec0604b029d9
Arthur Lazaryan <artlazaryan86@gmail.com>
2024-10-02 10:15:06 -0700

    Second message added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("This is a test program!");
+
+
+        // Will this show up when I push it again
+        System.out.println("Second message in the second commit"); 
     }
 }
